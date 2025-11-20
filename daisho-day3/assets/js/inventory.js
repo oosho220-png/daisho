634e9748cf83a610ebac98820f1695dfc9b1b8e2
oosho220-png <oosho220@gmail.com>
2025-11-20 14:51:03 +0900

    20251120v6

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -3,8 +3,8 @@
 //------------------------------
 const items = [
     {id:"D001",name:"オレンジジュース",price150,stock:20,minStock:5},
-    {id:"D002",name:"コーラ",price150,stock:12,minStock:5},
-    {id:"D003",name:"お茶",price120,stock:30,minStock:10},
+    {id:"D002",name:"コーラ",          price150,stock:12,minStock:5},
+    {id:"D003",name:"お茶",            price120,stock:30,minStock:10}
 ];
 
 //HTML で <tbody id="item-list"> と書いた部分を JavaScript から取得する
